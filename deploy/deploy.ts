12df9eefc22922dca1019c419ca2ab4576339ea5
Ishita Agarwal <ishitagrawal0207@gmail.com>
2024-10-22 11:59:02 +0530

    added deploy scripts and paymaster

---
+++ deploy/deploy.ts
@@ -0,0 +1,13 @@
+import { deployContract } from "./utils";
+
+export default async function () {
+    // Deploying the GeneralPaymaster contract
+    const Paymaster = "GeneralPaymaster";
+    const PaymasterArguments = [];
+    await deployContract(Paymaster, PaymasterArguments);
+
+    // Deploying the zkTune contract
+    const contractArtifactName = "zkTune";
+    const dAppArguments = [];
+    await deployContract(contractArtifactName, dAppArguments);
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,10 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    "zksync_sepolia": {
+      "url": "https://zksync-sepolia.g.alchemy.com/v2/20gKKYO3bnUx5QPEpz80cOFLtmSTxO2L",
+      "accounts": ["2d3193ac4c94e9babbc4333f68aaf69cc41ff3fbbbd50e3a2d83409bbc1952b1"]
+    }
+  }
 };
